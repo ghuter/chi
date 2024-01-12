767cfd49681f2c8fe76998774a4345dc8307196c
Floreal Risso <floreal.risso@univ-tlse3.fr>
2024-01-12 15:01:18 +0100

    tmp

--- parser.c
+++ parser.c
@@ -1,12 +1,18 @@
 #include "lib/token.h"
 
-#define TODO(_str) printf("TODO: " _str "\n")
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#define TODO(message) \
+    do { \
+        printf("TODO(%s): %s\n", TOSTRING(__LINE__), message); \
+    } while (0)
 
 static int line = 0;
 static int eoi = 0;
 FatArena fttok = {0};
 
-#define burnseparators(_t, _i)                 \
+#define BURNSEPARATORS(_t, _i)                 \
 	do {                                       \
 		while (1) {                            \
 			if (_t[_i] == NEWLINE) {           \
@@ -21,7 +27,22 @@ FatArena fttok = {0};
 		}                                      \
 	} while (0)                             
 
-#define ismonadicop(_op) (_op == SUB || _op == LNOT || _op == BNOT || _op == BXOR || _op == AT || _op == SIZEOF)
+#define ISEOE(_t, _eoe, _res)                  \
+do {                                           \
+	int _i = 0;                                \
+	_res = 0;                                  \
+	while (_eoe[_i] != UNDEFINED) {            \
+		if ((_t) == _eoe[_i]) {                \
+			_res = 1;                          \
+			break;                             \
+		}                                      \
+		_i++;                                  \
+	}                                          \
+} while (0)
+
+#define ISMONADICOP(_op) (_op == SUB || _op == LNOT || _op == BNOT || _op == BXOR || _op == AT || _op == SIZEOF)
+#define ISBINOP(_op) (_op >= MUL && _op <= LNOT)
+
 
 int
 parse_toplevel_fun(ETok *t)
@@ -78,7 +99,7 @@ parse_toplevel_struct(ETok *t)
 		fprintf(stderr, "INFO: member <%s> of type <%s>\n", (char*)ftptr(&ftident, ident), (char*)ftptr(&ftident, type));
 
 		if (t[i] == NEWLINE || t[i] == SEMICOLON) {
-			burnseparators(t, i);
+			BURNSEPARATORS(t, i);
 		} else {
 			fprintf(stderr, "ERR: line %d, expects a separator <NEWLINE> | <SEMICOLON>\n", line);
 			return 0;
@@ -105,23 +126,111 @@ parse_toplevel_enum(ETok *t)
 	return 0;
 }
 
+typedef enum {
+	ENONE,
+	ECST,
+	EMEM,
+	EBINOP,
+	EUNOP,
+	ECALL,
+	ERETURN,
+	NEXPR,
+} Expr;
+
+char *exprstr[NEXPR] = {
+	[ENONE]   = "ENONE",
+	[ECST]    = "ECST",
+	[EMEM]    = "EMEM",
+	[EBINOP]  = "EBINOP",
+	[EUNOP]   = "EUNOP",
+	[ECALL]   = "ECALL",
+	[ERETURN] = "ERETURN",
+};
+
 int
-parse_expression(ETok *t)
+parse_call(ETok *t)
 {
-	TODO("Expression");
+	const ETok eoe[3] = {COMMA, RPAREN, UNDEFINED};
 	int i = 0;
 
-	if (t[i] == IDENTIFIER)
-		TODO("IDENTIFIER");
-	if (t[i] == FLOAT)
-		TODO("FLOAT");
-	if (t[i] == INT)
-		TODO("INT");
-	if (t[i] == LITERAL)
-		TODO("LITERAL");
-	if (ismonadicop(t[i]))
-		TODO("MONADICOP");
+	if (t[i] == LPAREN) {
+	TODO("Function call");
+		fprintf(stderr, "INFO: line %d, <IDENTIFIER> is a fun call\n", line);
+		return 1;
+	}
 
+	return 0;
+}
+
+int
+parse_expression(ETok *t, const ETok *eoe)
+{
+	TODO("Expression");
+	fprintf(stderr, "INFO: t == %p\n", (void*)t);
+
+	int i = 0;
+	int ident = -1;
+	int op = -1;
+	Expr expr = ENONE;
+
+	switch (t[i]) {
+		case IDENTIFIER:
+			i++;
+			ident = t[i];
+			expr = EMEM;
+			i++;
+			if (parse_call(t + i) != 0) {
+				expr = ECALL;
+			}
+			break;
+
+		// Cst
+		case FLOAT:
+		case INT:
+		case LITERAL:
+			i++;
+			i++;
+			expr = ECST;
+			break;
+
+		// Unop
+		case SUB:    // fallthrough
+		case LNOT:   // fallthrough
+		case BNOT:   // fallthrough
+		case BXOR:   // fallthrough
+		case AT:     // fallthrough
+		case SIZEOF: 
+			i++;
+			expr = EUNOP;
+			i += parse_expression(t + i, eoe);
+			break;
+		default:
+			fprintf(stderr, "ERR: line %d, unexpected token <%s>\n", line, tokenstrs[t[i]]);
+			return 0;
+	}
+
+	// Here, we have parsed at least one expression.
+	// We need to check if it's the end.
+	int res = 0;
+
+	ISEOE(t[i], eoe, res);
+	if (res != 0) {
+		fprintf(stderr, "INFO: line %d, expression <%s>\n", line, exprstr[expr]);
+		return i;
+	}
+
+	if (ISBINOP(t[i])) {
+		op = t[i];
+		i++;
+		fprintf(stderr, "INFO: line %d, expression <%s> followed by a binop\n", line, exprstr[expr]);
+		fprintf(stderr, "---------------BINOP(%s, %s\n", tokenstrs[op], exprstr[expr]);
+		parse_expression(t + i, eoe);
+		fprintf(stderr,"---------------)\n");
+		return i;
+	}
+
+	fprintf(stderr, "i: %d\n", i);
+	fprintf(stderr, "ERR: line %d, the expression isn't followed by a binop, nor an end of expression, it's a <%s>\n", line, tokenstrs[t[i]]);
 	return 0;
 }
 
@@ -169,7 +278,8 @@ parse_toplevel_decl(ETok *t)
 		i++;
 		parse_toplevel_interface(t + i);
 	} else /* Expression */ {
-		parse_expression(t + i);
+		const ETok eoe[3] = {SEMICOLON, NEWLINE, UNDEFINED};
+		parse_expression(t + i, eoe);
 	}
 
 	// parse expression
@@ -186,7 +296,7 @@ parse_toplevel_decl(ETok *t)
 		i++;
 	}
 
-	fprintf(stderr, "INFO: line %d, declaration <%s> : <%s>", line, (char*) ftptr(&ftident, ident), ((type > 0) ? (char*) ftptr(&ftident, type) : "GUESS" ));
+	fprintf(stderr, "INFO: line %d, declaration <%s> : <%s>\n", line, (char*) ftptr(&ftident, ident), ((type > 0) ? (char*) ftptr(&ftident, type) : "GUESS" ));
 	return i;
 }
 
